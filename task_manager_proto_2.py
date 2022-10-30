e3ac1e8c556a44fe05c671253b0b4749800463b6
an0520a <an0520a@gmail.com>
2022-10-31 01:46:36 +0900

    2022_10_30

--- task_manager_proto_2.py
+++ task_manager_proto_2.py
@@ -1,4 +1,5 @@
 from concurrent.futures import process
+from re import I
 import sys
 import win32api
 import win32process
@@ -9,7 +10,7 @@ import win32gui
 import winnt
 import pywintypes
 import ctypes
-from ctypes import wintypes
+from ctypes import GetLastError, wintypes
 from ctypes import windll
 import elevate
 import os
@@ -32,7 +33,7 @@ class ProcessInfo:
     process_cpu_usgae : float = None
 
 @dataclass
-class ProcessInfoWithUsage:
+class PreprocessedProcessInfo:
     process_name : str = None
     process_pid : int = None
     process_owner : str = None
@@ -42,6 +43,7 @@ class ProcessInfoWithUsage:
     process_memory_info_dict : dict = field(default_factory=dict)
     process_memory_usage : int = None # 단위 : KB
     process_cpu_usgae : float = None
+    live : bool = False
 
 def kill_process(process_pid):
     hProc = win32api.OpenProcess(
@@ -83,11 +85,11 @@ def is_admin():
         return False
 
 # input value : https://msdn.microsoft.com/en-us/library/windows/desktop/bb530716(v=vs.85).aspx
-def set_privilege(szPrivilege):
-
+# 실패하면 False를 반환
+def set_privilege(szPrivilege : str):
     hToken = win32security.OpenProcessToken(
         win32api.GetCurrentProcess(),
-        win32con.TOKEN_ALL_ACCESS
+        win32con.TOKEN_ADJUST_PRIVILEGES | win32con.TOKEN_QUERY
     )
     # win32security.OpenProcessToken(HANDLE ProcessHandle, DWORD DesiredAccess)
         # Do it
@@ -97,6 +99,13 @@ def set_privilege(szPrivilege):
             # DesiredAccess : 토큰 접근 권한
         # return
             # 프로세스의 토큰을 반환 (Handle)
+
+    if hToken == 0:
+        return False
+
+
+
+
 
     luid = win32security.LookupPrivilegeValue(None, szPrivilege)
     # LUID LookupPrivilegeValueA(LPCSTR lpSystemName, LPCSTR lpName)
@@ -109,6 +118,14 @@ def set_privilege(szPrivilege):
                 # https://learn.microsoft.com/en-us/windows/win32/secauthz/enabling-and-disabling-privileges-in-c--
             # return
                 # 프로세스의 토큰을 반환 (Handle)
+
+    if luid == 0:
+        return False
+
+
+
+
+
 
     new_token_privilege = [(luid, win32con.SE_PRIVILEGE_ENABLED)]
     '''
@@ -125,7 +142,7 @@ def set_privilege(szPrivilege):
             - SE_PRIVILEGE_REMOVED : 권한을 제거하는데 사용
             - SE_PRIVILEGE_USED_FOR_ACCESS : 객체 또는 서비스에 엑세스하는데 사용
     '''
-    
+
     win32security.AdjustTokenPrivileges(hToken, win32con.FALSE, new_token_privilege)
     # AdjustTokenPrivileges(HANDLE TockenHandle, BOOL DisableAllPrivileges, TOKEN_PRIVILEGES NewState)
         # Do it
@@ -135,10 +152,16 @@ def set_privilege(szPrivilege):
             # DisableAllPrivileges : 토큰의 모든 권한을 비활성화 할지 정함.
             #                        TRUE라면 모든 권한을 비활성화. FALSE라면 권한을 수정
             # NewState : 새로 설정활 권한
+
+    if win32api.GetLastError():
+        return False
+
     win32api.CloseHandle(hToken)
 
+    return True
+
 # 현재 프로세스의 리스트와 정보를 ProcessInfo dataclass의 리스트로 반환 
-def get_process_info_list():
+def get_process_info_list() -> list[ProcessInfo] :
     process_info_list = []
     hProcessSnapshot = windll.kernel32.CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, None)
     process_entry_32 = PROCESSENTRY32()
@@ -170,7 +193,6 @@ def get_process_info_list():
 
             process_info.process_time_info_dict = win32process.GetProcessTimes(hProc)
             process_info.process_memory_info_dict = win32process.GetProcessMemoryInfo(hProc)
-            process_info.process_memory_usage = process_info.process_memory_info_dict["WorkingSetSize"] / 1024.0
 
             win32api.CloseHandle(hToken)
             win32api.CloseHandle(hProc)
@@ -186,20 +208,32 @@ def get_process_info_list():
 
     return process_info_list
 
-def preprocessing_process_info(prev_process_info_list : list[ProcessInfo], process_info_list : list[ProcessInfo]):
-    process_info_with_usage_list = []
+def preprocessing_process_info(prev_process_info_list : list[ProcessInfo], process_info_list : list[ProcessInfo]) -> list[PreprocessedProcessInfo]:
+    perproceseed_process_info_list = []
 
     process_info_list.sort()
     prev_process_info_list.sort()
 
-    for 
+    i = 0
+    j = 0
+
+    for i in range(0, process_info_list.len()):
+        perproceseed_process_info = PreprocessedProcessInfo()
+
+        if process_info_list[i].process_pid == prev_process_info_list[i].process_pid and process_info_list[i].process_time_info_dict["CreationTime"] == prev_process_info_list[i].process_pid["CreationTime"] :
+            perproceseed_process_info.process_memory_usage = process_info_list[i].process_memory_info_dict["WorkingSetSize"] / 1024.0
+
+            prev_process_time = prev_process_info_list[i].process_pid["KernelTime"] + prev_process_info_list[i].process_pid["UserTime"]
+            process_time = process_info_list[i].process_pid["KernelTime"] + process_info_list[i].process_pid["UserTime"]
 
     return None
 
 
 def main():
-    # elevate.elevate(show_console = True)
-    set_privilege(win32con.SE_DEBUG_NAME)
+    elevate.elevate(show_console = True)
+    if set_privilege(win32con.SE_DEBUG_NAME) == False:
+        print("error : can not set privilege")
+
     process_manager_update_time = 1
 
     prev_process_info_list = get_process_info_list()
@@ -216,6 +250,7 @@ def main():
         prev_process_info_list = process_info_list
 
         time.sleep(process_manager_update_time)
+        break
 
     os.system("pause")
 
